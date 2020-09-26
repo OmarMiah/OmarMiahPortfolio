3fcf4ac45984f0d05783d60c24c7069b0538a461
Omar Miah <45909860+omiahcs@users.noreply.github.com>
2020-09-26 04:20:56 -0400

    Adding Scroll bar (Fix Image position)

--- src/components/banner.js
+++ src/components/banner.js
@@ -12,16 +12,39 @@ const Banner = () => {
           }
         }
       }
+      blue: file(relativePath: { eq: "Blue.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      pikachu_hi_pokemon: file(relativePath: { eq: "pikachu_hi_pokemon.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
   return (
     <div className="banner">
       <div className="container">
         <div className="row">
+          <div className="side-image left">
+            <Img fluid={data.blue.childImageSharp.fluid} />
+          </div>
           <div className="main-text">Omar Miah</div>
           <div className="main-image">
+            <Img fluid={data.pikachu_hi_pokemon.childImageSharp.fluid} />
+          </div>
+          <div className="side-image right">
             <Img fluid={data.kkSlider.childImageSharp.fluid} />
           </div>
+        </div>
+        <div className="scroll">
+          <span>Scroll down</span>
         </div>
       </div>
     </div>
