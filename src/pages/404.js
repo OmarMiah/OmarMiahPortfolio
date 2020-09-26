8c8fd40ee054ec58fd8e186fa58b44829bc42c4c
Omar Miah <45909860+omiahcs@users.noreply.github.com>
2020-09-26 03:37:29 -0400

    portfolio moved

---
+++ src/pages/404.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const NotFoundPage = () => <p>404 Error: Page Not Found</p>
+
+export default NotFoundPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const SecondPage = () => <p>Second page</p>
+
+export default SecondPage
