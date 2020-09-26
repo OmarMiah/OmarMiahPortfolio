a4a567eb3af79b46226efbf56b4a1ed0dcee3d10
Omar Miah <45909860+omiahcs@users.noreply.github.com>
2020-09-26 03:56:02 -0400

    Centering name + Adding Images

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Banner = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      kkSlider: file(relativePath: { eq: "ACNH-KK-Header.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
-    <div className='banner'>
-      <div className='container'>
-        <div className='row'>
-          <div className='main-text'>Omar Miah</div>
+    <div className="banner">
+      <div className="container">
+        <div className="row">
+          <div className="main-text">Omar Miah</div>
+          <div className="main-image">
+            <Img fluid={data.kkSlider.childImageSharp.fluid} />
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Banner;
+export default Banner
